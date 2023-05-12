f18a7294469bccb873521c5c1f72ca8457c3ec4e
HankerLu <hankerlu1996@gmail.com>
2023-05-12 21:43:30 +0800

    Initial Commit

---
+++ Aicuite_mobile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Aicuite_mobile
+//
+//  Created by Hanker Lu on 2023/5/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
