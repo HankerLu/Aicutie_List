9cbe703432e38a4a1e5918b3e261653bede72341
HankerLu <hankerlu1996@gmail.com>
2023-05-17 16:30:38 +0800

    NEW: modify insert order

--- Aicuite_mobile/ViewController.swift
+++ Aicuite_mobile/ViewController.swift
@@ -25,12 +25,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     var counter_of_button_clicked = 0
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print("section = \(section)")
+        // print("section = \(section)")
         return tbl_array.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        print("indexPath.row = \(indexPath.row)")
+        // print("indexPath.row = \(indexPath.row)")
         let cell = tableView.dequeueReusableCell(withIdentifier: "tblcell1", for: indexPath) 
         cell.textLabel?.text = tbl_array[indexPath.row]
         return cell
@@ -59,8 +59,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         counter_of_button_clicked += 1
         mylabel.text = "current cell num: \(counter_of_button_clicked)"
         
-        // add a tableview cell for mytblview
-        tbl_array.append("tbl data \(counter_of_button_clicked)")
+        // add a tableview cell to the top of mytblview
+        tbl_array.insert("tbl data \(counter_of_button_clicked)", at: 0)
+        // tbl_array.append("tbl data \(counter_of_button_clicked)")
 
         //get the last cell
         mytblview.reloadData()
@@ -69,6 +70,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         button.setTitle("Button", for: .normal)
         // button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
         
+        //get the top cell if mytblview
+        // let cell = mytblview.cellForRow(at: IndexPath(row: 0, section: 0))
         let cell = mytblview.cellForRow(at: IndexPath(row: tbl_array.count-1, section: 0))
 //        if cell != nil {
 //            print("cell is not nil")
@@ -78,6 +81,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 //        }
         print("view X Y \(view.frame.origin.x) \(view.frame.origin.y)")
         print("cell X Y \(cell?.frame.origin.x) \(cell?.frame.origin.y)")
+        print("table W H \(mytblview.frame.size.width) \(mytblview.frame.size.height)")
         
         
         let nextButton = UIButton()
@@ -93,8 +97,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         nextButton.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             //set nextButton's X Y to the cell's X Y
-            nextButton.centerXAnchor.constraint(equalTo: cell!.leftAnchor, constant: 15),
-            nextButton.centerYAnchor.constraint(equalTo: cell!.centerYAnchor),
+            nextButton.centerXAnchor.constraint(equalTo: mytblview.leftAnchor, constant: 15),
+            nextButton.centerYAnchor.constraint(equalTo: mytblview.centerYAnchor),
             nextButton.widthAnchor.constraint(equalToConstant: 25),
             nextButton.heightAnchor.constraint(equalToConstant: 25)
          ])
