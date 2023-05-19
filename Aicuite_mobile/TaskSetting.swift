ab67687de1af603c285ef84ae0ac08512a91173a
HankerLu <hankerlu1996@gmail.com>
2023-05-19 21:53:43 +0800

    NEW: add subVC for time setting

--- Aicuite_mobile/TaskSetting.swift
+++ Aicuite_mobile/TaskSetting.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class TaskSettingScreen: UIViewController {
 
+    @IBOutlet var button_timeset_confirm: UIButton!
+    @IBOutlet var button_timeset_cancel: UIButton!
+    @IBOutlet var button_date_picker: UIDatePicker!
+    @IBOutlet var button_time_hms_enable_switch: UISwitch!
+
+    @IBAction func button_timeset_confirm_clicked(_ sender: Any) {
+        print("button_timeset_confirm_clicked")
+    }
+
+    @IBAction func button_timeset_cancel_clicked(_ sender: Any) {
+        print("button_timeset_cancel_clicked")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemMint
